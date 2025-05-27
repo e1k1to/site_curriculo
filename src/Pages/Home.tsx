3164b19bfc19c292f9a5924e3e3b687685a8e825
e1k1to <eikimillian@protonmail.com>
2025-05-27 17:11:59 -0300

    add sessao sobre mim

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,15 +1,28 @@
 import '../assets/global.css'
-import logo from '../assets/logo.jpg'
+import foto from '../assets/foto.webp'
 import { GlobalHeader } from '../Pages/Header.tsx'
 
 export function Home() {
     return (
         <>
         <GlobalHeader/>
-        <div class="container apresentacao">
-            <div class="foto">
-            </div>
-            <div class="sobremim">
+        <div class="container">
+            <div class="apresentacao">
+                <div class="dv-foto">
+                    <img src={foto} class="foto-eu"/>
+                </div>
+                <div class="dv-desc">
+                    <div class="sobre-mim">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                    </p>
+                        <div class="sobre-mim-2">
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt 
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
         </>
